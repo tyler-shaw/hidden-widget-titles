2ea240d4a4eb94bc682908acf86570fa43ead4b3
Tyler Shaw <tylershaw80@yahoo.com>
2014-08-25 02:10:53 -0400

    Added initial widget title removal functionality.

--- hidden-widget-titles.php
+++ hidden-widget-titles.php
@@ -5,3 +5,18 @@
  * Version: 0.1.0
  * Author: Tyler Shaw
  */
+
+// Anonymous function requiring PHP 5.3 or greater.
+add_filter('widget_title', function($title) {
+	
+	$title_check = trim($title);
+	
+	if($title_check[0] == '[' && $title_check[strlen($title_check) - 1] == ']') {
+		$title = '';
+		return $title;
+	}
+	else {
+		return $title;
+	}
+	
+});
