079dde6bebca8c6e49efd9e16ee8eb5146550ca4
Tyler Shaw <tylershaw80@yahoo.com>
2014-08-25 02:03:33 -0400

    Added main plugin file.

---
+++ hidden-widget-titles.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Plugin Name: Hidden Widget Titles
+ * Description: Allows select widget titles to display in the admin panel but not on your site.
+ * Version: 0.1.0
+ * Author: Tyler Shaw
+ */
