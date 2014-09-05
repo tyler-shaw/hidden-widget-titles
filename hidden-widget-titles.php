92e2bfd2000be90a70b1954a56b1a7ba947698a9
Tyler Shaw <tylershaw80@yahoo.com>
2014-09-04 22:02:45 -0400

    Updated version number in main file in preparation of launch to WordPress plugin directory.

--- hidden-widget-titles.php
+++ hidden-widget-titles.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Hidden Widget Titles
  * Description: Allows select widget titles to display in the admin panel but not on your site.
- * Version: 0.1.0
+ * Version: 1.0.0
  * Author: Tyler Shaw
  */
 
